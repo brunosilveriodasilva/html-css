9933c3aef6fed5c922c06069006b1b1987b5047b
brunosilveriodasilva <brunosilveriodasilva0@gmail.com>
2025-10-09 16:17:00 -0300

    atualização na store

--- projetos/projto-store/script.js
+++ projetos/projto-store/script.js
@@ -4,6 +4,12 @@ const items = document.querySelectorAll('.item');
 const dots = document.querySelectorAll('.dot');
 const numbersIndicator = document.querySelector('.numbers');
 const list = document.querySelectorAll('.list');
+const smButtons = document.querySelectorAll('.btn');
+const urls = [
+    'https://www.apple.com/br/watch/?afid=p240%7Cgo~cmp-22996552056~adg-186624687753~ad-778108676591_kwd-52218226~dev-c~ext-~prd-~mca-~nt-search&cid=wwa-br-kwgo-watch-core-watchfam-watchfamily_hero_avail_100825-AppleWatch-Core-Exact-AppleWatch-Exact-apple+watch',
+    'https://www.apple.com/br/airpods/?afid=p240%7Cgo~cmp-23006674489~adg-188742252161~ad-778112783723_kwd-50006603375~dev-c~ext-~prd-~mca-~nt-search&cid=wwa-br-kwgo-airpods-noncore_airpodsfamily-airpodsfamily-airpodsfamily_hero_avail_100825-AirPods-AirPods-airpods',
+    'https://www.apple.com/br/airpods-max/?afid=p240%7Cgo~cmp-23006674489~adg-188742252161~ad-778112783723_kwd-50006603375~dev-c~ext-286846637157~prd-~mca-~nt-search&cid=wwa-br-kwgo-airpods-noncore_airpodsfamily-airpodsfamily-airpodsfamily_hero_avail_100825-AirPods-AirPods-airpods',
+];
 
 let active = 0;
 const total = items.length;
@@ -44,7 +50,7 @@ function stopInterval() {
 prevButton.addEventListener('click', () => {
     update(-1);
     stopInterval();
-    startInterval();   
+    startInterval();  
 });
 
 nextButton.addEventListener('click', () => {
@@ -54,3 +60,13 @@ nextButton.addEventListener('click', () => {
 });
 
 startInterval();
+
+smButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        if (index === active) { 
+            const url = urls[active];
+            if (url) window.open(url, '_blank');
+        }
+    })
+});
+
